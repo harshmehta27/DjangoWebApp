cf816ab19646c356f23630ac99816c76a9874f29
Harsh Mehta <40580524+harshmehta27@users.noreply.github.com>
2019-02-16 17:17:33 -0500

    Add files via upload

---
+++ Batch_File.bat
@@ -0,0 +1,3 @@
+cd /d %~dp0
+python manage.py runserver
+start http://localhost:8000
---
+++ install_libraries.bat
@@ -0,0 +1,6 @@
+python -m pip install Django
+python -m pip install geopy
+python -m pip install plotly
+python -m pip install requests
+pytohn -m pip install pymongo
+python -m pip install pandas
